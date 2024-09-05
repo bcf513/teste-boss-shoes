7e844c207295fc0b4b5ac19ff628a0b4040fa7fe
Mauricio Molinari <programador02@grupom1.com.br>
2024-09-05 10:48:46 -0300

    Feat: Imagens de produtos em Product

--- src/app/(components)/Product.tsx
+++ src/app/(components)/Product.tsx
@@ -3,28 +3,31 @@
 import { useCartContext } from "@/providers/Cart";
 import React from "react";
 import { IoBagOutline } from "react-icons/io5";
+import Image from "next/image";
 
 function Product({
-  productId,
-  title,
-  description,
-  price,
+  product,
 }: Readonly<{
-  productId: number;
-  title: string;
-  description: string;
-  price: number;
+  product: Product;
 }>) {
   const { addProduct } = useCartContext();
+
+  const { id, name, description, photo, price } = product;
   return (
     <div className="flex flex-col items-center pe-4 rounded-b-3xl p-4 bg-red-900">
-      <h1>Foto</h1>
-      <h3>{title}</h3>
+      <Image
+        className="h-50 w-50"
+        src={photo}
+        alt={id.toString()}
+        width={500}
+        height={500}
+      />
+      <h3>{name}</h3>
       <p>{description}</p>
       <div className="flex justify-between w-full items-center">
         <h3>R$ {price.toString()}</h3>
         <div className="border-spacing-4 border-solid border-white rounded-lg border-2 p-2">
-          <IoBagOutline onClick={() => addProduct(productId)} />
+          <IoBagOutline onClick={() => addProduct(id)} />
         </div>
       </div>
     </div>
--- src/app/(components)/ProductsSection.tsx
+++ src/app/(components)/ProductsSection.tsx
@@ -9,13 +9,7 @@ function ProductsSection() {
 
       <div className="grid grid-cols-3 gap-3">
         {products.map((product) => (
-          <Product
-            key={product.id}
-            productId={product.id}
-            title={product.name}
-            description={product.description}
-            price={product.price}
-          />
+          <Product key={product.id} product={product} />
         ))}
       </div>
     </div>
