8f91728c30bb0cdeae363e3a4df79a12245ccd22
Mauricio Molinari <programador02@grupom1.com.br>
2024-09-06 10:32:01 -0300

    Feat: Estilo de modal de opções em tela responsiva

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,8 +2,8 @@
 
 import Link from "next/link";
 import React, { useState } from "react";
-import { HiMagnifyingGlass } from "react-icons/hi2";
-import { IoBagOutline } from "react-icons/io5";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { PiHandbagSimpleBold } from "react-icons/pi";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { useCartContext } from "@/providers/Cart";
 import CartModal from "../modals/CartModal";
@@ -27,9 +27,9 @@ function Header() {
         <div className="hidden lg:flex items-center gap-5 rounded-l-3xl p-4 backdrop-blur-sm bg-white/30 ">
           <Link href="">HOME</Link>
           <h1>OUR PRODUCTS</h1>
-          <HiMagnifyingGlass />
+          <FaMagnifyingGlass />
           <div className="relative">
-            <IoBagOutline onClick={() => setModalOpened(true)} />
+            <PiHandbagSimpleBold onClick={() => setModalOpened(true)} />
             {cart.length > 0 && (
               <h1 className="absolute -right-2 -bottom-4 rounded-full bg-red-500 text-xs p-1">
                 {cart.reduce((acc, product) => acc + product.quantity, 0)}
@@ -38,28 +38,36 @@ function Header() {
           </div>
         </div>
       </div>
-      <div className="lg:hidden">
-        <GiHamburgerMenu onClick={() => setMenuOpened(!menuOpened)} />
-      </div>
-      {menuOpened && (
-        <div className="absolute top-16 right-0 z-10 bg-white shadow-lg rounded-lg p-4 flex flex-col items-start lg:hidden">
-          <Link href="" className="py-2" onClick={() => setMenuOpened(false)}>
-            HOME
-          </Link>
-          <h1 className="py-2" onClick={() => setMenuOpened(false)}>
-            OUR PRODUCTS
-          </h1>
-          <HiMagnifyingGlass className="py-2" />
-          <div className="relative py-2" onClick={() => setModalOpened(true)}>
-            <IoBagOutline />
-            {cart.length > 0 && (
-              <h1 className="absolute -right-2 -bottom-4 rounded-full bg-red-500 text-xs p-1">
-                {cart.reduce((acc, product) => acc + product.quantity, 0)}
-              </h1>
-            )}
-          </div>
+      <div className="relative h-full">
+        <div className="lg:hidden">
+          <GiHamburgerMenu onClick={() => setMenuOpened(!menuOpened)} />
         </div>
-      )}
+        {menuOpened && (
+          <div className="absolute top-0 right-0 z-10 bg-white shadow-lg rounded-lg p-6 flex flex-col items-start lg:hidden text-black  whitespace-nowrap ">
+            <Link href="" className="py-2" onClick={() => setMenuOpened(false)}>
+              HOME
+            </Link>
+            <h1 className="py-2" onClick={() => setMenuOpened(false)}>
+              OUR PRODUCTS
+            </h1>
+
+            <div className="flex w-full justify-evenly items-center">
+              <FaMagnifyingGlass />
+              <div
+                className="relative py-2"
+                onClick={() => setModalOpened(true)}
+              >
+                <PiHandbagSimpleBold />
+                {cart.length > 0 && (
+                  <h1 className="absolute -right-2 -bottom-4 rounded-full bg-red-500 text-xs p-1">
+                    {cart.reduce((acc, product) => acc + product.quantity, 0)}
+                  </h1>
+                )}
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
       {modalOpened && <CartModal closeModal={() => setModalOpened(false)} />}
     </header>
   );
