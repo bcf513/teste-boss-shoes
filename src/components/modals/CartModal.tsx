d3de240ef50c24f4074e2493b0c0b7be9ff3a771
Mauricio Molinari <programador02@grupom1.com.br>
2024-09-06 11:36:12 -0300

    Refactor: Mudança para table em CartModal

--- src/components/modals/CartModal.tsx
+++ src/components/modals/CartModal.tsx
@@ -9,54 +9,80 @@ function CartModal({ closeModal }: Readonly<{ closeModal: () => void }>) {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-md relative text-black">
+      <div className="bg-white rounded-lg shadow-lg p-3 md:p-6 lg:p-6 w-fit max-w-md relative text-black">
         {cart.length ? (
           <>
-            <h1>Your Cart</h1>
-            <div className="flex justify-between">
-              <h3>Product</h3>
-              <h3>Price</h3>
-            </div>
-            <div className="flex flex-col gap-2">
-              {cart.map((product) => (
-                <div
-                  className="flex justify-between items-center"
-                  key={product.id}
-                >
-                  <Image
-                    className="h-10 w-10 drop-shadow-xl rounded-md bg-gray-200"
-                    src={product.photo}
-                    alt={product.id.toString()}
-                    width={500}
-                    height={500}
-                  />
-                  <h3 className="w-24">{product.name}</h3>
-                  <div className="flex gap-2">
-                    <button
-                      className="w-6 rounded-full bg-gray-200 hover:bg-red-400"
-                      onClick={() => removeProduct(product.id)}
-                    >
-                      -
-                    </button>
-                    <span>{product.quantity}</span>
-                    <button
-                      className="w-6 rounded-full bg-gray-200 hover:bg-green-400"
-                      onClick={() => addProduct(product.id)}
-                    >
-                      +
-                    </button>
-                  </div>
-                  <h3>{product.price * product.quantity}</h3>
-                </div>
-              ))}
-              <h1 className="flex justify-end">
-                Total: R$
-                {cart.reduce(
-                  (acc, curr) => acc + curr.price * curr.quantity,
-                  0
-                )}
-              </h1>
-            </div>
+            <h1 className="text-center font-bold">Your Cart</h1>
+            <table className="w-full border-collapse border-gray-200 text-xs md:text-base">
+              <thead>
+                <tr>
+                  <th className="p-1 md:p-2 lg:p-2 text-left">Product</th>
+                  <th className="p-1 md:p-2 lg:p-2 text-left">Price</th>
+                  <th className="p-1 md:p-2 lg:p-2 text-left">Quantity</th>
+                  <th className="p-1 md:p-2 lg:p-2 text-left">Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cart.map((product) => (
+                  <tr key={product.id}>
+                    <td className="flex flex-col items-center gap-4 p-2 md:flex-row lg:flex-row">
+                      <Image
+                        className="h-10 w-10 drop-shadow-xl rounded-md bg-gray-200"
+                        src={product.photo}
+                        alt={product.name}
+                        width={50}
+                        height={50}
+                      />
+                      {product.name}
+                    </td>
+                    <td className=" p-2 whitespace-nowrap">
+                      <p className="w-fit h-fit">
+                        R$ {product.price.toFixed(2).replace(".", ",")}
+                      </p>
+                    </td>
+                    <td className="p-2 flex items-center justify-center gap-2 ">
+                      <button
+                        className="w-6 h-6 rounded-full bg-gray-200 hover:bg-red-400"
+                        onClick={() => removeProduct(product.id)}
+                      >
+                        -
+                      </button>
+                      <span className="text-center">{product.quantity}</span>
+                      <button
+                        className="w-6 h-6 rounded-full bg-gray-200 hover:bg-green-400"
+                        onClick={() => addProduct(product.id)}
+                      >
+                        +
+                      </button>
+                    </td>
+
+                    <td className=" p-2 whitespace-nowrap">
+                      R${" "}
+                      {(product.price * product.quantity)
+                        .toFixed(2)
+                        .replace(".", ",")}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td colSpan={3} className=" p-2 text-right font-bold">
+                    Total:
+                  </td>
+                  <td className=" p-2 whitespace-nowrap">
+                    R${" "}
+                    {cart
+                      .reduce(
+                        (acc, curr) => acc + curr.price * curr.quantity,
+                        0
+                      )
+                      .toFixed(2)
+                      .replace(".", ",")}
+                  </td>
+                </tr>
+              </tfoot>
+            </table>
           </>
         ) : (
           <h1 className="flex justify-center">Your cart is empty!</h1>
